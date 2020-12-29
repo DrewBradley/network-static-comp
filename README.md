34ba5f5a9b264abfc7e45844fc229f5e876014af
Drew Bradley <williamandrewb@gmail.com>
2020-12-29 13:00:15 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Network Request Practice
+
+## Clone down repo
+- ```git clone ```
+- ```cd network-request-exercises```
+- ```npm i```
+- ```npm start```
+
+## To run the application
+- Open the ```index.html``` in your browser
+- To add data to the API fill in the corresponding fields and click ```Add User```
+- To find a user or delete a user, you only need to select the corresponding ID.
+
+## Images
+![Add User](https://media.giphy.com/media/ITLQybVp70mGQPSqUz/giphy.gif)
+![Find User](https://media.giphy.com/media/D5QI7eDoC8MWvmwUSR/giphy.gif)
---
+++ server.js
@@ -0,0 +1,138 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 3001;
+const app = express();
+const { users, sportTeams, animals } = require('./data.js');
+
+app.locals = {
+  title: 'Network Request Practice',
+  users,
+  sportTeams,
+  animals
+}
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/api/v1/users', (req, res) => {
+  res.status(200).json(app.locals.users);
+});
+
+app.post('/api/v1/users', (req, res) => {
+  const newUser = req.body;
+
+  for (let requiredParameter of ['id', 'name', 'status', 'interests']) {
+    if (!newUser[requiredParameter]) {
+      return res.status(422).json({
+        message: `You are missing a required parameter of ${requiredParameter}`
+      });
+    }
+  }
+
+  const { id, name, status, interests } = newUser;
+
+  app.locals.users = [...app.locals.users, { id, name, status, interests }];
+
+  return res.status(201).json({ id, name, status, interests });
+});
+
+app.delete('/api/v1/users/:id', (req, res) => {
+  const { id } = req.params;
+  const match = app.locals.users.find(user => user.id == id);
+
+  if (!match) {
+    return res.status(404).json({
+      message: `No user found with an id of ${id}`
+    });
+  }
+
+  const filteredUsers = app.locals.users.filter(user => user.id != id);
+
+  app.locals.users = filteredUsers;
+
+  return res.status(200).json(app.locals.users);
+});
+
+app.get('/api/v1/sport-teams', (req, res) => {
+  res.status(200).json(app.locals.sportTeams);
+});
+
+app.post('/api/v1/sport-teams', (req, res) => {
+  const newSportTeam = req.body;
+
+  for (let requiredParameter of ['id', 'name', 'head_coach', 'sport']) {
+    if (!newSportTeam[requiredParameter]) {
+      return res.status(422).json({
+        message: `You are missing a required parameter of ${requiredParameter}`
+      });
+    }
+  }
+
+  const { id, name, head_coach, sport } = newSportTeam;
+
+  app.locals.sportTeams = [...app.locals.sportTeams, { id, name, head_coach, sport }];
+
+  return res.status(201).json({ id, name, head_coach, sport });
+});
+
+app.delete('/api/v1/sport-teams/:id', (req, res) => {
+  const { id } = req.params;
+  const match = app.locals.sportTeams.find(sportTeam => sportTeam.id == id);
+
+  if (!match) {
+    return res.status(404).json({
+      message: `No sport team found with an id of ${id}`
+    });
+  }
+
+  const filteredSportTeams = app.locals.sportTeams.filter(user => user.id != id);
+
+  app.locals.sportTeams = filteredSportTeams;
+
+  return res.status(200).send(app.locals.sportTeams);
+});
+
+app.get('/api/v1/animals', (req, res) => {
+  res.status(200).json(app.locals.animals);
+});
+
+app.post('/api/v1/animals', (req, res) => {
+  const newAnimal = req.body;
+
+  for (let requiredParameter of ['id', 'name', 'diet', 'fun_fact']) {
+    if (!newAnimal[requiredParameter]) {
+      return res.status(422).json({
+        message: `You are missing a required parameter of ${requiredParameter}`
+      });
+    }
+  }
+
+  const { id, name, diet, fun_fact } = newAnimal;
+
+  app.locals.animals = [...app.locals.animals, { id, name, diet, fun_fact }];
+
+  return res.status(201).json({ id, name, diet, fun_fact });
+});
+
+app.delete('/api/v1/animals/:id', (req, res) => {
+  const { id } = req.params;
+  const match = app.locals.animals.find(sportTeam => sportTeam.id == id);
+
+  if (!match) {
+    return res.status(404).json({
+      message: `No animal found with an id of ${id}`
+    });
+  }
+
+  const filteredAnimals = app.locals.animals.filter(user => user.id != id);
+
+  app.locals.animals = filteredAnimals;
+
+  return res.status(200).json(app.locals.animals);
+});
+
+app.listen(port, () => {
+  console.log(`${app.locals.title} is now running on http://localhost:${port} !`)
+});
+
